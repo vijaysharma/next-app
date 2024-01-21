9da56f3602aaf69e1270d84b2b7a4106801f8bd4
Vijay Sharma <Vijay.Sharma@causeway.com>
2024-01-21 20:16:41 +0530

    Add SASS support to the project.

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import AddToCart from '../AddToCart'
+import React from 'react';
+import AddToCart from '../AddToCart';
+import styles from './ProductCard.module.scss';
 
 const ProductCard = () => {
   return (
-    <div>
-        <h1>Product Card</h1>
-        <AddToCart/>
+    <div className={styles.card}>
+      <h1>Product Card</h1>
+      <AddToCart />
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
